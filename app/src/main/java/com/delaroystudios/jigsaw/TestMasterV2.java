24096744b13fb6cc1ee4043efd6417f4604187b3
masterUNG <phrombutr@gmail.com>
2017-01-11 23:16:36 +0700

    เพิ่มหน้าแรก

---
+++ app/src/main/java/com/delaroystudios/jigsaw/TestMasterV2.java
@@ -0,0 +1,13 @@
+package com.delaroystudios.jigsaw;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestMasterV2 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_master_v2);
+    }
+}
