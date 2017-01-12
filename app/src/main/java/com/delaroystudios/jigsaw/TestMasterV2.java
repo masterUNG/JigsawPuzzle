99fa5576d5257fcc5e56fd96ac06dd28cc3eadc5
masterUNG <phrombutr@gmail.com>
2017-01-12 23:22:32 +0700

    update Last

--- app/src/main/java/com/delaroystudios/jigsaw/TestMasterV2.java
+++ app/src/main/java/com/delaroystudios/jigsaw/TestMasterV2.java
@@ -1,7 +1,9 @@
 package com.delaroystudios.jigsaw;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class TestMasterV2 extends Activity {
 
@@ -9,5 +11,10 @@ public class TestMasterV2 extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_master_v2);
+    }   // Main Method
+
+    public void clickGoToMain(View view) {
+        startActivity(new Intent(TestMasterV2.this, MainActivity.class));
     }
-}
+
+}   // Main Class
